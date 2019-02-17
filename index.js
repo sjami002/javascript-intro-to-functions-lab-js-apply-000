fe6b6ce60ec020c6cc165663c83ab94db63955b5
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-17 18:53:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!"
   }
 }
-
-}
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too."
