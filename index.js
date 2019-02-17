240a70c2d24839ac3c0075e00a59432a1c4c03ba
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-17 18:50:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,8 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   }
 }
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too"
+  }
+}
