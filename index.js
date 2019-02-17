6fd73cca7e598ca60bb67e1c94cc328167fec585
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-17 18:57:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can\'t hear you!"
-  }
-}
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }else if (string === "I love you, Grandma.") {
     return "I love you, too."
+  }else {
+    return "I can\'t hear you!"
   }
 }
